2a3fe3e170d1c0c66b66a7f21f6bae97d1f04fa8
VeejaLiu <veejaliu@gmail.com>
2024-08-07 10:58:31 +0800

    refactor: Add functions for executing raw queries in models/index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,1 +1,41 @@
-export {};
+import { Logger } from '../lib/logger';
+import { sequelize } from './db-config';
+import { QueryTypes } from 'sequelize';
+
+const logger = new Logger(__filename);
+
+export async function doRawQuery(query: string) {
+    try {
+        const res = await sequelize.query(query, {
+            type: QueryTypes.SELECT,
+            raw: true,
+        });
+        return res;
+    } catch (e) {
+        logger.error(`[doRawQuery] ${e}`);
+    }
+}
+
+export async function doRawUpdate(query: string) {
+    try {
+        const res = await sequelize.query(query, {
+            type: QueryTypes.UPDATE,
+            raw: true,
+        });
+        return res;
+    } catch (e) {
+        logger.error(`[doRawUpdate] ${e}`);
+    }
+}
+
+export async function doRawInsert(query: string) {
+    try {
+        const res = await sequelize.query(query, {
+            type: QueryTypes.INSERT,
+            raw: true,
+        });
+        return res;
+    } catch (e) {
+        logger.error(`[doRawInsert] ${e}`);
+    }
+}
