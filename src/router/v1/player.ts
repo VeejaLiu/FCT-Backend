923efb57e6cd9073fdb68f335c0f8a18117743a5
VeejaLiu <veejaliu@gmail.com>
2024-08-08 16:22:35 +0800

    remove useless router

--- src/router/v1/player.ts
+++ src/router/v1/player.ts
@@ -35,66 +35,6 @@ router.get('/trends', async (req: any, res) => {
 });
 
 /**
- * Create or update player
- */
-router.post('/single/:playerID', async (req: any, res) => {
-    try {
-        const playerID = req.params.playerID;
-        logger.info(`[API_LOGS][/player] req.body=${JSON.stringify(req.body)}`);
-        const { saveUID, currentDate, overallrating, potential } = req.body;
-
-        logger.info(
-            `[API_LOGS][/player] playerID=${playerID}, saveUID=${saveUID}, currentDate=${currentDate}, overallrating=${overallrating}, potential=${potential}`,
-        );
-
-        if (!playerID || !overallrating || !potential) {
-            res.status(400).send('Bad Request');
-            return;
-        }
-
-        // query first
-        const queryRes: any[] = await doRawQuery(`SELECT * FROM player WHERE player_id = ${playerID} limit 1`);
-        if (queryRes.length === 0) {
-            // insert
-            const result = await doRawInsert(
-                `INSERT INTO player (player_id, overallrating, potential)
-                 VALUES (${playerID}, ${overallrating}, ${potential})`,
-            );
-            logger.info(
-                `[API_LOGS][/player] Created new player: playerID=${playerID}, overallrating=${overallrating}, potential=${potential}`,
-            );
-            return res.send({
-                playerID: playerID,
-                message: 'Created successfully',
-            });
-        } else {
-            // update
-            const result = await doRawQuery(
-                `UPDATE player
-                 SET overallrating=${overallrating},
-                     potential=${potential}
-                 WHERE player_id = ${playerID}`,
-            );
-            if (
-                parseInt(queryRes[0].overallrating) !== parseInt(overallrating) ||
-                parseInt(queryRes[0].potential) !== parseInt(potential)
-            ) {
-                logger.info(
-                    `[API_LOGS][/player] Updated player: playerID=${playerID}, overallrating=${queryRes[0].overallrating}->${overallrating}, potential=${queryRes[0].potential}->${potential}`,
-                );
-            }
-            return res.send({
-                playerID: playerID,
-                message: 'Updated successfully',
-            });
-        }
-    } catch (e) {
-        logger.error(`[API_LOGS][/player] ${e}`);
-        res.status(500).send('Internal Server Error');
-    }
-});
-
-/**
  * Create or update players in bulk
  */
 router.post('/bulk', async (req: any, res) => {
