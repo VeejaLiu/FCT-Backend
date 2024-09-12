cc943c008f313cd3aed7f10ea2cae67bf393926d
VeejaLiu <veejaliu@gmail.com>
2024-09-12 16:33:35 +0800

    Refactor down-avatar script to use a constant for the player ID file name

--- src/scripts/down-avatar.ts
+++ src/scripts/down-avatar.ts
@@ -3,7 +3,9 @@ import fs from 'fs';
 import path from 'path';
 
 const basePath = '/Users/weijialiu/fcd-data/';
+const playerIdFileName = 'id.txt';
 const BATCH_SIZE = 100;
+
 function saveImage(imageBase64: string, i: number) {
     // Split images into folders, each folder contains 1000 images
     const folderIndex = Math.floor(i / 1000);
@@ -37,8 +39,8 @@ async function main() {
     console.log('Start');
     // Get start id from file if exists: c.txt
     let startId = 0;
-    if (fs.existsSync(path.join(basePath, 'c.txt'))) {
-        startId = parseInt(fs.readFileSync(path.join(basePath, 'c.txt'), 'utf-8'));
+    if (fs.existsSync(path.join(basePath, playerIdFileName))) {
+        startId = parseInt(fs.readFileSync(path.join(basePath, playerIdFileName), 'utf-8'));
     }
 
     const promises = [];
@@ -47,7 +49,7 @@ async function main() {
         if (promises.length >= BATCH_SIZE) {
             await Promise.all(promises);
             promises.length = 0;
-            fs.writeFileSync(path.join(basePath, 'c.txt'), i.toString());
+            fs.writeFileSync(path.join(basePath, playerIdFileName), i.toString());
         }
         // Save current id to file
     }
