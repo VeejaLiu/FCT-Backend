3749d096867b35362b926b5df293dbc0012b2496
VeejaLiu <veejaliu@gmail.com>
2024-11-01 09:46:20 +0800

    add new function - registerUserWithLock()

--- src/general/user/register.ts
+++ src/general/user/register.ts
@@ -3,8 +3,28 @@ import { Logger } from '../../lib/logger';
 import { refreshSecretKey } from './refresh-secret-key';
 import { UserModel } from '../../models/schema/UserDB';
 import { Op } from 'sequelize';
+import AsyncLock from 'async-lock';
 
 const logger = new Logger(__filename);
+const lock = new AsyncLock();
+
+export async function registerUserWithLock({
+    username,
+    email,
+    password,
+}: {
+    username: string;
+    email: string;
+    password: string;
+}): Promise<{
+    success: boolean;
+    message: string;
+    data?: any;
+}> {
+    return lock.acquire('registrationLock', async () => {
+        return registerUser({ username, email, password });
+    });
+}
 
 export async function registerUser({
     username,
