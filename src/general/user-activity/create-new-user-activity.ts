5a38e4189fc883a57e4c551ac56ddb6fec0c98c9
VeejaLiu <veejaliu@gmail.com>
2024-12-10 18:50:00 +0800

    remove log of createNewUserActivity

--- src/general/user-activity/create-new-user-activity.ts
+++ src/general/user-activity/create-new-user-activity.ts
@@ -6,7 +6,7 @@ const logger = new Logger(__filename);
 export async function createNewUserActivity({ userID }: { userID: string }): Promise<void> {
     try {
         const now = new Date();
-        logger.info(`[createNewUserActivity] userID: ${userID}, time: ${now}`);
+        // logger.info(`[createNewUserActivity] userID: ${userID}, time: ${now}`);
 
         await UserActivityModel.create({ user_id: userID, activity_time: now });
     } catch (e) {
