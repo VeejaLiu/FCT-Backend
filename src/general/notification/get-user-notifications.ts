0d9e93a8317acf78c10036737812cc3833b34550
VeejaLiu <veejaliu@gmail.com>
2024-12-10 11:54:35 +0800

    add try catch

--- src/general/notification/get-user-notifications.ts
+++ src/general/notification/get-user-notifications.ts
@@ -1,9 +1,17 @@
 import { UserNotificationModel } from '../../models/schema/UserNotificationDB';
+import { Logger } from '../../lib/logger';
+
+const logger = new Logger(__filename);
 
 export async function getUserNotifications({ userId, gameVersion }: { gameVersion: number; userId: any }) {
-    const res = await UserNotificationModel.getAllNotifications({
-        userId: userId,
-        gameVersion: gameVersion,
-    });
-    return res;
+    try {
+        const res = await UserNotificationModel.getAllNotifications({
+            userId: userId,
+            gameVersion: gameVersion,
+        });
+        return res;
+    } catch (error) {
+        logger.error(error);
+        return [];
+    }
 }
