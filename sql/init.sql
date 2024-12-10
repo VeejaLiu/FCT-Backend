c8b3b64b233639d20ba9b761c8fdb3a20c2a0360
VeejaLiu <veejaliu@gmail.com>
2024-12-10 19:13:33 +0800

    add new index

--- sql/init.sql
+++ sql/init.sql
@@ -194,7 +194,8 @@ CREATE TABLE `user_notification`
     `create_time`        datetime   DEFAULT CURRENT_TIMESTAMP,
     `update_time`        datetime   DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
-    KEY `idx_user_id` (`user_id`)
+    KEY `idx_user_id` (`user_id`),
+    KEY `idx_in_game_date` (`in_game_date`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_general_ci;
