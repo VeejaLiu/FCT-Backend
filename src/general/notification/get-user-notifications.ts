b2726b755f3d2d223b937270b5240800a4a1e0f6
VeejaLiu <veejaliu@gmail.com>
2024-12-10 10:44:48 +0800

    refactor getUserNotifications to use getAllNotifications method

--- src/general/notification/get-user-notifications.ts
+++ src/general/notification/get-user-notifications.ts
@@ -1,13 +1,9 @@
 import { UserNotificationModel } from '../../models/schema/UserNotificationDB';
 
 export async function getUserNotifications({ userId, gameVersion }: { gameVersion: number; userId: any }) {
-    const res = await UserNotificationModel.findAll({
-        where: {
-            user_id: userId,
-            game_version: gameVersion,
-            is_deleted: 0,
-        },
-        raw: true,
+    const res = await UserNotificationModel.getAllNotifications({
+        userId: userId,
+        gameVersion: gameVersion,
     });
     return res;
 }
